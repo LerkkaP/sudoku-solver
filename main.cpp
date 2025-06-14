7facacdbe3a5ba7c0a0060b0eb85c7a53a473c7d
Erik Peteri <peteri.erik@gmail.com>
2025-06-14 09:42:56 +0300

    Create function to check that a given 3x3 square is valid

--- main.cpp
+++ main.cpp
@@ -3,17 +3,18 @@
 void printGrid(const int (&grid)[9][9]);
 bool isValidRow(int grid[9][9], int row, int col);
 bool isValidColumn(int grid[9][9], int row, int col);
+bool isValidSquare(int grid[9][9], int row, int col);
 
 int main()
 {
     int sudokuGrid[9][9] = {
         {1, 2, 3, 4, 5, 6, 7, 8, 9},
-        {2, 2, 3, 4, 5, 6, 7, 8, 9},
-        {3, 2, 3, 4, 5, 6, 7, 8, 9},
+        {4, 5, 9, 4, 5, 6, 7, 8, 9},
+        {9, 8, 7, 4, 5, 6, 7, 8, 9},
         {4, 2, 3, 4, 5, 6, 7, 8, 9},
-        {5, 2, 3, 4, 5, 6, 7, 8, 9},
+        {5, 2, 3, 2, 2, 3, 7, 8, 9},
         {6, 2, 3, 4, 5, 6, 7, 8, 9},
-        {7, 2, 3, 4, 5, 6, 7, 8, 9},
+        {7, 2, 3, 7, 8, 9, 7, 8, 9},
         {8, 2, 3, 4, 5, 6, 7, 8, 9},
         {9, 2, 3, 4, 5, 6, 7, 8, 9}
     };
@@ -42,6 +43,30 @@ bool isValidColumn(int grid[9][9], int row, int col)
     for (int i = 0; i < row; i++) {
         if (grid[i][col] == cell) {
             return false;
+        }
+    }
+    return true;
+}
+
+// checks that the square is valid according to the constraints
+bool isValidSquare(int grid[9][9], int row, int col)
+{
+    int cell {grid[row][col]};
+
+    int squareStartRow { (row / 3) * 3 };
+    int squareEndRow { squareStartRow + 2 };
+
+    int squareStartColumn { (col / 3) * 3 };
+    int squareEndColumn { squareStartColumn + 2 };
+
+    for (int i = squareStartRow; i <= squareEndRow; i++) {
+        for (int j = squareStartColumn; j <= squareEndColumn; j++) {
+            if (i == row && j == col) {
+                continue;
+            }
+            if (grid[i][j] == cell) {
+                return false;
+            }
         }
     }
     return true;
