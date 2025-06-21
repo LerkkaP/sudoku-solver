ba4a7fee0a4c7a23b2152068ed0ff7cb08ae977c
Erik Peteri <peteri.erik@gmail.com>
2025-06-21 19:29:09 +0300

    Define the constraint checking helper functions inside anonymous namespace for internal linkage

--- include/constraints.h
+++ include/constraints.h
@@ -3,9 +3,6 @@
 
 #include <array>
 
-bool isValidRow(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num);
-bool isValidColumn(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num);
-bool isValidSquare(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num);
 bool isValid(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num);
 
 #endif
--- src/constraints.cpp
+++ src/constraints.cpp
@@ -1,56 +1,58 @@
 #include <array>
 #include "constraints.h"
 
-// checks that the row is valid according to the constraints
-bool isValidRow(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num) 
-{
-    for (int j = 0; j < 9; ++j) {
-        // if the column is same as the cell we are comparing to, skip
-        if (j == col) {
-            continue;
-        }
-        if (grid[row][j] == num) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// checks that the column is valid according to the constraints
-bool isValidColumn(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num)
-{
-    for (int i = 0; i < 9; ++i) {
-        // if the row is same as the cell we are comparing to, we skip
-        if (i == row) {
-            continue;
-        }
-        if (grid[i][col] == num) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// checks that the square is valid according to the constraints
-bool isValidSquare(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num)
-{
-    int squareStartRow { (row / 3) * 3 };
-    int squareEndRow { squareStartRow + 2 };
-
-    int squareStartColumn { (col / 3) * 3 };
-    int squareEndColumn { squareStartColumn + 2 };
-
-    for (int i = squareStartRow; i <= squareEndRow; ++i) {
-        for (int j = squareStartColumn; j <= squareEndColumn; ++j) {
-            if (i == row && j == col) {
+namespace {
+    // checks that the row is valid according to the constraints
+    bool isValidRow(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num) 
+    {
+        for (int j = 0; j < 9; ++j) {
+            // if the column is same as the cell we are comparing to, skip
+            if (j == col) {
                 continue;
             }
-            if (grid[i][j] == num) {
+            if (grid[row][j] == num) {
                 return false;
             }
         }
+        return true;
     }
-    return true;
+
+    // checks that the column is valid according to the constraints
+    bool isValidColumn(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num)
+    {
+        for (int i = 0; i < 9; ++i) {
+            // if the row is same as the cell we are comparing to, we skip
+            if (i == row) {
+                continue;
+            }
+            if (grid[i][col] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // checks that the square is valid according to the constraints
+    bool isValidSquare(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num)
+    {
+        int squareStartRow { (row / 3) * 3 };
+        int squareEndRow { squareStartRow + 2 };
+
+        int squareStartColumn { (col / 3) * 3 };
+        int squareEndColumn { squareStartColumn + 2 };
+
+        for (int i = squareStartRow; i <= squareEndRow; ++i) {
+            for (int j = squareStartColumn; j <= squareEndColumn; ++j) {
+                if (i == row && j == col) {
+                    continue;
+                }
+                if (grid[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
 
 bool isValid(const std::array<std::array<int, 9>, 9> &grid, int row, int col, int num) {
